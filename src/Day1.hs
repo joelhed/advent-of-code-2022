db02359d5ffac3af0a059cc50ca8784b0753c2af
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2022-12-01 19:21:17 +0100

    Solve day 1 part 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,9 +1,22 @@
 module Day1 (day1) where
 
 import Lib
-import Data.Char (toUpper)
+
+-- Split a list at the appearance of a given item. This removes said item.
+splitAtItem :: (Eq a) => a -> [a] -> [[a]]
+splitAtItem _ [] = []
+splitAtItem item xs =
+    let (group, rest) = break (== item) xs
+    in case rest of
+        [] -> group:[]
+        rest -> group:(splitAtItem item (tail rest))
+
+thisPart1 :: String -> String
+thisPart1 input =
+    show $ maximum $ map sum groups
+    where groups = map (map read) $ splitAtItem "" $ lines input :: [[Int]]
 
 day1 :: Day
-day1 = Day { part1 = map toUpper
+day1 = Day { part1 = thisPart1 --maximum . map (sum . (map read)) . splitAtItem "" . lines
            , part2 = Nothing
            }
