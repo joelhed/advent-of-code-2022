86b1c692d33594fe8466c9f7b7e972754167df0b
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2022-12-01 19:36:36 +0100

    Solve day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,6 +1,7 @@
 module Day1 (day1) where
 
 import Lib
+import Data.List (sort)
 
 -- Split a list at the appearance of a given item. This removes said item.
 splitAtItem :: (Eq a) => a -> [a] -> [[a]]
@@ -11,12 +12,19 @@ splitAtItem item xs =
         [] -> group:[]
         rest -> group:(splitAtItem item (tail rest))
 
+getElfInventories :: String -> [[Int]]
+getElfInventories = map (map read) . splitAtItem "" . lines
+
 thisPart1 :: String -> String
-thisPart1 input =
-    show $ maximum $ map sum groups
-    where groups = map (map read) $ splitAtItem "" $ lines input :: [[Int]]
+thisPart1 = show . maximum . map sum . getElfInventories
+
+threeLargest :: [Int] -> [Int]
+threeLargest = take 3 . reverse . sort
+
+thisPart2 :: String -> String
+thisPart2 = show . sum . threeLargest . map sum . getElfInventories
 
 day1 :: Day
 day1 = Day { part1 = thisPart1 --maximum . map (sum . (map read)) . splitAtItem "" . lines
-           , part2 = Nothing
+           , part2 = (Just thisPart2)
            }
