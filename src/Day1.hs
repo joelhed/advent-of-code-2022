d20728133bb471242bcf5b70a7ed714d9d0b8baf
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2022-12-01 20:15:39 +0100

    Implement alternative solution to day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,7 @@
 module Day1 (day1) where
 
 import Lib
-import Data.List (sort)
+import Data.List (sort, insert)
 
 -- Split a list at the appearance of a given item. This removes said item.
 splitAtItem :: (Eq a) => a -> [a] -> [[a]]
@@ -18,8 +18,31 @@ getElfInventories = map (map read) . splitAtItem "" . lines
 thisPart1 :: String -> String
 thisPart1 = show . maximum . map sum . getElfInventories
 
+-- This is a simple solution, but could be inefficient due to sorting being O(n*log n).
+threeLargestWithSort :: [Int] -> [Int]
+threeLargestWithSort = take 3 . reverse . sort
+
+-- We should only really need to pass through the list once.
+-- I'll try to implement this functionally.
+-- This could be done by using a recursive pattern where we pass along the current
+-- highest as a parameter to a hidden recursive function.
+threeLargestWithRec :: [Int] -> [Int]
+threeLargestWithRec input =
+    let
+        threeLargestRec :: [Int] -> [Int] -> [Int]
+        threeLargestRec currentMax [] = currentMax
+        threeLargestRec currentMax (x:xs) =
+            threeLargestRec newMax xs
+            where newMax = tail $ insert x currentMax
+        (firstThree, rest) = splitAt 3 input
+    in
+        threeLargestRec firstThree rest
+
 threeLargest :: [Int] -> [Int]
-threeLargest = take 3 . reverse . sort
+threeLargest = threeLargestWithRec
+
+-- Turns out, both solutions take approximately 0,26 seconds to complete.
+-- No practical difference.
 
 thisPart2 :: String -> String
 thisPart2 = show . sum . threeLargest . map sum . getElfInventories
