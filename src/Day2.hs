04a2c29c4ef41273fceaaf63908438d4aa825466
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2022-12-02 13:40:44 +0100

    Change day 2 from pattern matching to case exprs

--- src/Day2.hs
+++ src/Day2.hs
@@ -7,47 +7,52 @@ data Result = Win | Loss | Draw
 type Round = (Play, Play)
 
 charToPlay :: Char -> Play
-charToPlay 'A' = Rock
-charToPlay 'B' = Paper
-charToPlay 'C' = Scissors
-charToPlay 'X' = Rock
-charToPlay 'Y' = Paper
-charToPlay 'Z' = Scissors
-charToPlay _ = error "unrecognised play"
+charToPlay char = case char of
+    'A' -> Rock
+    'B' -> Paper
+    'C' -> Scissors
+    'X' -> Rock
+    'Y' -> Paper
+    'Z' -> Scissors
+    _   -> error "unrecognised play"
 
 parseRound :: String -> Round
 parseRound row =
     (charToPlay opponent , charToPlay me)
     where opponent = head row
-          me = last row
+          me       = last row
 
 playScore :: Play -> Int
-playScore Rock = 1
-playScore Paper = 2
-playScore Scissors = 3
+playScore play = case play of
+    Rock     -> 1
+    Paper    -> 2
+    Scissors -> 3
 
 didWin :: Round -> Result
-didWin (Rock, Paper) = Win
-didWin (Paper, Scissors) = Win
-didWin (Scissors, Rock) = Win
-didWin (opponent, me)
-    | opponent == me = Draw
-    | otherwise      = Loss
+didWin round = case round of
+    (Rock, Paper)                   -> Win
+    (Paper, Scissors)               -> Win
+    (Scissors, Rock)                -> Win
+    (opponent, me) | opponent == me -> Draw
+                   | otherwise      -> Loss
 
 winScore :: Result -> Int
-winScore Win = 6
-winScore Draw = 3
-winScore Loss = 0
+winScore result = case result of
+    Win  -> 6
+    Draw -> 3
+    Loss -> 0
 
 calculateScore :: Round -> Int
 calculateScore round = (winScore $ didWin round) + (playScore $ snd round)
 
 -- Part 2
+
 charToResult :: Char -> Result
-charToResult 'X' = Loss
-charToResult 'Y' = Draw
-charToResult 'Z' = Win
-charToResult _ = error "unexpected result"
+charToResult char = case char of
+    'X' -> Loss
+    'Y' -> Draw
+    'Z' -> Win
+    _   -> error "unexpected result"
 
 parseExpectedResult :: String -> (Play, Result)
 parseExpectedResult row =
@@ -56,18 +61,21 @@ parseExpectedResult row =
           expected = last row
 
 findRightPlay :: (Play, Result) -> Play
-findRightPlay (opponent, Draw) = opponent
-findRightPlay (Rock, Win)      = Paper
-findRightPlay (Paper, Win)     = Scissors
-findRightPlay (Scissors, Win)  = Rock
-findRightPlay (Rock, Loss)     = Scissors
-findRightPlay (Paper, Loss)    = Rock
-findRightPlay (Scissors, Loss) = Paper
+findRightPlay expected = case expected of
+    (opponent, Draw) -> opponent
+    (Rock, Win)      -> Paper
+    (Paper, Win)     -> Scissors
+    (Scissors, Win)  -> Rock
+    (Rock, Loss)     -> Scissors
+    (Paper, Loss)    -> Rock
+    (Scissors, Loss) -> Paper
 
 roundFromExpectedResult :: (Play, Result) -> Round
 roundFromExpectedResult expected = (fst expected, findRightPlay expected)
 
 day2 :: Day
-day2 = Day { part1 = show . sum . map (calculateScore . parseRound) . lines
-           , part2 = Just $ show . sum . map (calculateScore . roundFromExpectedResult . parseExpectedResult) . lines
+day2 = Day { part1 = let lineToScore = calculateScore . parseRound
+                     in show . sum . map lineToScore . lines
+           , part2 = let lineToScore = calculateScore . roundFromExpectedResult . parseExpectedResult
+                     in Just $ show . sum . map lineToScore . lines
            }
