c9f7979baa92cddd7047f525da5de57797c730ea
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2022-12-09 16:53:10 +0100

    Solve day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -2,7 +2,7 @@ module Day8 (day8) where
 
 import Data.Char (isDigit, digitToInt)
 import Data.List (transpose)
-import GHC.Arr (listArray, Array (..), range, (!), bounds, accumArray, foldrElems)
+import GHC.Arr (array, listArray, Array (..), range, (!), bounds, accumArray, foldrElems, assocs, elems)
 import Lib
 
 type Position = (Int, Int)
@@ -28,7 +28,7 @@ visibleTrees forest
           south = transpose east
           north = map reverse south
           initialScannerState :: ScannerState
-          initialScannerState = ((0, 0), True, -1) -- this will be ignored in the output
+          initialScannerState = ((0, 0), True, -1)
           getPosAndVisibility :: ScannerState -> (Position, Bool)
           getPosAndVisibility (pos, visibility, _) = (pos, visibility)
           visibilityScanner :: ScannerState -> Position -> ScannerState
@@ -51,8 +51,29 @@ _prettyForest toChar forest = unlines ( map (toChar . (forest!))
                                       )
     where (highestY, highestX) = snd . bounds $ forest
 
+scenicScores :: Forest Int -> Forest Int
+scenicScores forest
+    = array (bounds forest) . map posAndScore $ assocs forest
+    where (highestY, highestX) = snd . bounds $ forest
+          posAndScore pair@(pos, _) = (pos, score pair)
+          score :: (Position, Int) -> Int
+          score (pos, height) = foldr (*) 1
+                              . map (treesVisible height . tailOrEmpty . ($ pos))
+                              $ [upFrom, downFrom, leftFrom, rightFrom]
+          rightFrom pos@(_y, _x) = range (pos, (_y, highestX))
+          downFrom  pos@(_y, _x) = range (pos, (highestY, _x))
+          leftFrom  pos@(_y, _x) = reverse . range $ ((_y, 0), pos)
+          upFrom    pos@(_y, _x) = reverse . range $ ((0, _x), pos)
+          tailOrEmpty [] = []
+          tailOrEmpty xs = tail xs
+          treesVisible height
+            = let countWithFinalTree (smaller, rest) = length smaller
+                                                     + if rest == [] then 0 else 1
+              in countWithFinalTree . span (< height) . map (forest!)
+            
+
 day8 :: Day
 day8 = Day { part1 = show . foldrElems ((+) . boolToNum) (0 :: Int) . visibleTrees . parseForest
-           , part2 = Nothing
+           , part2 = Just $ show . maximum . elems . scenicScores . parseForest
            }
     where boolToNum x = if x then 1 else 0
